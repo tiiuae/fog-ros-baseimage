b5ab2f8cda3ce18f54064a51ffd88d0e820c34f5
Manuel Segarra-Abad <manuel.segarra-abad@unikie.com>
2022-04-26 10:14:36 +0300

    fakeroot not needed when using docker builds

--- builder/packaging/package.sh
+++ builder/packaging/package.sh
@@ -126,8 +126,8 @@ bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro ${ROS_
     && bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro ${ROS_DISTRO} --process-template-files -i ${build_nbr}${git_version_string} \
     && sed -i 's/^\tdh_shlibdeps.*/& --dpkg-shlibdeps-params=--ignore-missing-info/g' debian/rules \
 		&& sed -i "s/\=\([0-9]*\.[0-9]*\.[0-9]*\*\)//g" debian/control \
-    && fakeroot debian/rules clean \
-    && fakeroot debian/rules "binary --parallel" || exit 1
+    && debian/rules clean \
+    && debian/rules "binary --parallel" || exit 1
 
 echo "[INFO] Clean up."
 
